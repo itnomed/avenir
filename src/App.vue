338de7a14531bb8a92b2097339e21b6cf8e168e8
Aibek Omorov <bashatgroup777@gmail.com>
2021-04-15 17:50:06 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template> 
+  <div class="container" @click:="changeNameHandler">
+  {{name}} {{surname}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      name: "John",
+      surname: "Snower"
+    }
+  },
+  methods:{
+    changeNameHandler (){
+      if(this.surname === "Snow"){
+        return this.name = "Alice";
+      }
+    }
+  },
+  m
+}
+</script>
+<style>
+.container{
+  color: red;
+  margin: auto;
+  width: 80%;
+  font-size: 28px;
+
+}
+</style>
